c27db3236172876a5da855cafd7ac6f324b85b8e
Richard K <72702198+richieeKee@users.noreply.github.com>
2025-06-26 21:45:37 +0000

    Erste Karte sichtbar, alle Änderungen in outdated-Folder verbucht

--- src/correlaid/Karte_Messstationen/tagesverlauf.csv.py
+++ src/correlaid/Karte_Messstationen/tagesverlauf.csv.py
@@ -7,18 +7,52 @@
 # Also diesen Datensatz hier aufbereiten. Und die Daten idealerweise direkt aus Open Data ziehen,
 # damit sich die Website automatisch aktualisiert, wenn es die Daten tun.
 
-import pandas
+import pandas as pd
 import sys
 
 # ---
 
 correlaid_csv = "https://github.com/CorrelAid/smart-green-city-konstanz/raw/refs/heads/main/maites_notebooks/karte_vgl_stationen/finalerdf_ws"
-
+ws_24_path = (
+    "/workspaces/smart-green-city-konstanz-dashboard/correlaid/ws_compl_summer_24.csv"
+)
 day = "2024-07-31"
 
+"""
+Altes skript um vom der Ursprungsdatei zu ws_compl_summer zu kommen
+# ---
+ws_24 = pd.read_csv(ws_24_path)
+# change datetime column to timezone-naive values (utc as argument since some entries have ms and somes dont) 
+# From entries with +01 and +02 the hours are subtracted respectively
+ws_24['dateobserved'] = pd.to_datetime(ws_24['dateobserved'],utc=True, format='ISO8601').dt.tz_convert(None)
+
+#new variables: date, time, hour & week_of_year
+ws_24['date'] = ws_24['dateobserved'].dt.date
+ws_24['time'] = ws_24['dateobserved'].dt.time
+ws_24['hour'] = ws_24['dateobserved'].dt.hour
+
+#calculate median values grouped by location, date & hour 
+#drop irrelevant columns& round results to two decimal places
+ws_hour = ws_24.drop(columns=['entity_id', 'dateobserved', 'time', 'entity_id']).groupby(['name', 'date', 'hour']).median().round(2).reset_index()
+ws_hour.head()
+
+#vollständigen dataframe erstellen, fehlende werte = nan
+all_possible_combinations = pd.MultiIndex.from_product(
+    [ws_hour['name'].unique(), pd.to_datetime(ws_hour['date']).unique(), range(24)],
+    names=['name', 'date', 'hour'])
+
+ws_compl = ws_hour.set_index(['name', 'date', 'hour']).reindex(all_possible_combinations, fill_value=np.nan)
+ws_compl = ws_compl.reset_index()
+
+#insg. nur zwei sehr auffällige werte (sehr sicher falsche messergebnisse) -> durch nan ersetzen
+ws_compl.loc[~((ws_compl['z_score'].abs() <= 3) | ws_compl['z_score'].isna()), 'temperature'] = np.nan
+
+#europapark raus
+ws_compl_summer = ws_compl[ws_compl['name'] != 'Europapark']
+"""
 # ---
 
-full = pandas.read_csv(correlaid_csv, index_col=0).reset_index(drop=True)
+full = pd.read_csv(correlaid_csv, index_col=0).reset_index(drop=True)
 del full["datetime"]
 full.rename(
     columns={
